d1f33be6e323478b8d6a19612843b2ee81b464c3
unknown <shaikhshahbaz4022@gmail.com>
2023-06-02 18:07:50 +0530

    Siblings To Siblings practice Done

--- React/sprint-1/day-5/Counter-Learn/counter/src/App.js
+++ React/sprint-1/day-5/Counter-Learn/counter/src/App.js
@@ -29,31 +29,61 @@ import './App.css';
 //   );
 // }
 
-function Counter() { //  child To Parent
+// function Counter() { //  child To Parent
+//   let [count, setCount] = useState(0)
+
+//   function Child({ count, addfun, Subfun }) {
+//     return (
+//       <div>
+//         <h1>{count}</h1>
+//         <button onClick={addfun} >+</button>
+//         <button onClick={Subfun} >-</button>
+
+
+//       </div>
+//     )
+//   }
+//   const addfun = () => {
+//     setCount(count + 1)
+//   }
+//   const subfun = () => {
+//     setCount(count - 1)
+//   }
+//   return (
+//     <div className='App' >
+//       <Child count={count} addfun={addfun} Subfun={subfun} />
+//     </div>
+//   )
+// }
+function Button({ text, Onclick , count }) {
+  return <button disabled={count<=0} onClick={Onclick}>{text}</button>
+}
+function Count({ count }) {
+  return <h1>{count}</h1>
+}
+function Siblings() {
+
   let [count, setCount] = useState(0)
-
-  function Child({ count, addfun, Subfun }) {
-    return (
-      <div>
-        <h1>{count}</h1>
-        <button onClick={addfun} >+</button>
-        <button onClick={Subfun} >-</button>
-
-
-      </div>
-    )
-  }
-  const addfun = () => {
+  const Add = () => {
     setCount(count + 1)
   }
-  const subfun = () => {
+  const Sub = () => {
     setCount(count - 1)
   }
+
   return (
-    <div className='App' >
-      <Child count={count} addfun={addfun} Subfun={subfun} />
+    <div className='App'>
+      <h1>Siblings To Siblings</h1>
+      <p>state is in parent , button and count is child <br/>,they are given by props</p>
+      <Count count={count} />
+      <Button text="-" Onclick={Sub} count={count} />
+      <Button text="+" Onclick={Add} />
+
+
+
     </div>
   )
 }
 
-export default Counter;
+
+export default Siblings;
