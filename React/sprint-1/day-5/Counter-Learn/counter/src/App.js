f5ca15cb4943610fc012ede3c793ca4ae0f21664
unknown <shaikhshahbaz4022@gmail.com>
2023-06-02 17:46:08 +0530

    Child to Parent

--- React/sprint-1/day-5/Counter-Learn/counter/src/App.js
+++ React/sprint-1/day-5/Counter-Learn/counter/src/App.js
@@ -2,31 +2,58 @@
 import { useState } from 'react';
 import './App.css';
 
-function Counter() {
+// function Counter() {
+//   let [count, setCount] = useState(0)
+
+
+//   function DisplayCount({ count }) {
+//     return <p>{count}</p>
+//   }
+
+//   const addfun = () => {
+//     setCount(count + 1)
+//   }
+
+//   const subfun = () => {
+//     setCount(count - 1)
+//   }
+
+
+//   return (
+//     <div className="App">
+//       <DisplayCount count={count} />
+//       <button onClick={addfun} >+</button>
+//       <button onClick={subfun} >-</button>
+
+//     </div>
+//   );
+// }
+
+function Counter() { //  child To Parent
   let [count, setCount] = useState(0)
 
+  function Child({ count, addfun, Subfun }) {
+    return (
+      <div>
+        <h1>{count}</h1>
+        <button onClick={addfun} >+</button>
+        <button onClick={Subfun} >-</button>
 
-  function DisplayCount({ count }) {
-    return <p>{count}</p>
+
+      </div>
+    )
   }
-
   const addfun = () => {
     setCount(count + 1)
   }
-
   const subfun = () => {
     setCount(count - 1)
   }
-
-
   return (
-    <div className="App">
-      <DisplayCount count={count} />
-      <button onClick={addfun} >+</button>
-      <button onClick={subfun} >-</button>
-
+    <div className='App' >
+      <Child count={count} addfun={addfun} Subfun={subfun} />
     </div>
-  );
+  )
 }
 
 export default Counter;
--- React/sprint-1/day-5/Counter-Learn/counter/src/index.js
+++ React/sprint-1/day-5/Counter-Learn/counter/src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Counter from './App';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<Counter />);
+root.render( <App />
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
