a988f1840a2c229ddcfcddf826f673a248342ae0
unknown <shaikhshahbaz4022@gmail.com>
2023-06-04 16:40:59 +0530

    Done Timer

--- React/sprint-1/day-7/timer/src/App.js
+++ React/sprint-1/day-7/timer/src/App.js
@@ -3,14 +3,16 @@ import { useState } from 'react';
 import './App.css';
 import Timer from './components/timer';
 
-function App() {
 
+function App() {
+  
   let [toggle, setToggle] = useState(false)
   return (
     <div className="App">
-     { toggle &&  <Timer />} 
-     {/* if toggle and timer is true then only you start timer */}
-      <button onClick={()=>setToggle(!toggle)} >{toggle ? "Hide Timer" : "Show Timer"}</button>
+    
+      {/* if toggle and timer is true then only you start timer */}
+      {toggle && <Timer />}
+      <button onClick={() => setToggle(!toggle)} >{toggle ? "Hide Timer" : "Show Timer"}</button>
     </div>
   );
 }
