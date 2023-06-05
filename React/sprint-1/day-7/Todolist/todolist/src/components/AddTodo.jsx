cd89641993084aab04688e9e26e6d0ce6de97a4b
unknown <shaikhshahbaz4022@gmail.com>
2023-06-05 16:26:38 +0530

    Data Displayed from Json Server

--- React/sprint-1/day-7/Todolist/todolist/src/components/AddTodo.jsx
+++ React/sprint-1/day-7/Todolist/todolist/src/components/AddTodo.jsx
@@ -1,4 +1,7 @@
-function AddTodo(){
-    return <></>
+function AddTodo({title,status}){
+    return <div style={{border:"1px solid black" , margin:"10px"}}>
+        <p>{title}</p>
+        <p>{status ? "Compleated" : "Not Compleated"}</p>
+    </div>
 }
 export default AddTodo
--- React/sprint-1/day-7/Todolist/todolist/src/components/Todo.jsx
+++ React/sprint-1/day-7/Todolist/todolist/src/components/Todo.jsx
@@ -1,4 +1,43 @@
-function Todo(){
-    return <>Todo</>
+import { useEffect, useState } from "react"
+import AddTodo from "./AddTodo"
+
+function Todo() {
+    let [todos, setTodos] = useState([])
+    let [loading, setLoading] = useState(false)
+    let [Error, setError] = useState(false)
+
+    useEffect(()=>{
+        fetchAndDisplay()
+    },[])
+
+    async function fetchAndDisplay() {
+        try {
+            setLoading(true)
+            let data = await fetch("http://localhost:8080/todos")
+            let result = await data.json()
+            setTodos(result)
+            setLoading(false)
+        } catch (error) {
+            setError(true)
+            setLoading(false)
+        }
+    }
+
+    if(loading){
+        return <p>Loading....</p>
+    }
+    if (Error){
+        return <p>Error is occured</p>
+    }
+
+    return <div>
+        {
+            todos.map((ele)=>(
+                <AddTodo key={ele.id} {...ele}/>
+
+            ))
+        }
+
+    </div>
 }
 export default Todo
