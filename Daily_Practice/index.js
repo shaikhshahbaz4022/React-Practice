09171a53c935011c50759edb4e07e2445a479055
unknown <shaikhshahbaz4022@gmail.com>
2023-07-02 15:59:56 +0530

    Practice of Classes

--- Daily_Practice/index.js
+++ Daily_Practice/index.js
@@ -1,18 +1,35 @@
 // learning closures
-function outer() {
-  let vari = "hello"
-  function Inner() {
-    console.log(vari)
-  }
-  return Inner
-}
-let temp = outer()
-console.log(temp())
+// function outer() {
+//   let vari = "hello"
+//   function Inner() {
+//     console.log(vari)
+//   }
+//   return Inner
+// }
+// let temp = outer()
+// console.log(temp())
 
-function Multiply(x) {
-  return function(y) {
-    console.log(x * y)
+// function Multiply(x) {
+//   return function(y) {
+//    return (x * y)
+//   }
+// }
+// var newf = Multiply(2)
+// console.log(newf(5))
+
+class Animal{
+  constructor(name,age){
+    this.name = name;
+    this.age = age
   }
 }
-var newf = Multiply(2)
-console.log(newf(5))
+// let dog = new Animal("Puppu",32)
+// console.log(dog)
+class cat extends Animal{
+  constructor(name,age ,address){
+    super(name,age)
+    this.address = address
+  }
+}
+let catty = new cat("dolly",32,"UK")
+console.log(catty)
