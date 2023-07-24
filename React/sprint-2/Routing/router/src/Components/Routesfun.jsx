d782787202cc740aed99db51428aed18ae70d0cb
unknown <shaikhshahbaz4022@gmail.com>
2023-07-24 20:23:45 +0530

    reqres api used ...completed

--- React/sprint-2/Routing/router/src/Components/Routesfun.jsx
+++ React/sprint-2/Routing/router/src/Components/Routesfun.jsx
@@ -3,6 +3,7 @@ import Home from "../Pages/Home";
 import About from "../Pages/About";
 import Services from "../Pages/Services";
 import Users from "../Pages/Users";
+import UserSinglePage from "../Pages/UsersSinglePage";
 function Routesfun() {
   return (
     <Routes>
@@ -10,6 +11,7 @@ function Routesfun() {
       <Route path="/About" element={<About />}></Route>
       <Route path="/Services" element={<Services />}></Route>
       <Route path="/Users" element={<Users />}></Route>
+      <Route path="/Users/:userID" element={<UserSinglePage />}></Route>
     </Routes>
   );
 }
--- React/sprint-2/Routing/router/src/Pages/UsersSinglePage.jsx
+++ React/sprint-2/Routing/router/src/Pages/UsersSinglePage.jsx
@@ -1,6 +1,50 @@
-// function UserSinglePage(){
-//     return (
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 
-//     )
-// }
-// export default UserSinglePage
+function UserSinglePage() {
+  const { userID } = useParams();
+  const [res, setRes] = useState({});
+  const [loader, setloader] = useState(false);
+  const [err, seterr] = useState(false);
+  const { data, support } = res;
+  function getdata(url) {
+    return fetch(url).then((res) => res.json());
+  }
+  async function displayData(id) {
+    try {
+      setloader(true);
+      let data = await getdata(`https://reqres.in/api/users/${id}`);
+      console.log(data);
+      setloader(false);
+      setRes(data);
+    } catch (error) {
+      console.log(error);
+      seterr(true);
+      setloader(false);
+    }
+  }
+
+  useEffect(() => {
+    displayData(userID);
+  }, [userID]);
+
+  if (loader) {
+    return <h1>Loading...</h1>;
+  }
+  if (err) {
+    return <h1>Error....</h1>;
+  }
+  return (
+    <div style={{ margin: "10px", padding: "10px", border: "1px solid black" }}>
+      <img src={data?.avatar} alt={data?.first_name} />
+      <h2>
+        {data?.first_name} {data?.last_name}
+      </h2>
+      <p>{data?.email}</p>
+      <hr />
+      <p>{support?.url}</p>
+      <p>{support?.text}</p>
+    </div>
+  );
+}
+export default UserSinglePage;
